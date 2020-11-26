61eb4b8f6a473afa8fc8dcba8c4d8644ce93bacc
Edmilson Andrade <edmilisonwork@gmail.com>
2020-11-26 14:43:34 +0100

    Removed unnecessary statement

--- dbseed.php
+++ dbseed.php
@@ -16,19 +16,6 @@
                 REFERENCES person(id)
                 ON DELETE SET NULL
         );
-
-        INSERT INTO person
-            (id, firstname, lastname, firstparent_id, secondparent_id)
-        VALUES
-            (1, 'Krasimir', 'Hristozov', null, null),
-            (2, 'Maria', 'Hristozova', null, null),
-            (3, 'Masha', 'Hristozova', 1, 2),
-            (4, 'Jane', 'Smith', null, null),
-            (5, 'John', 'Smith', null, null),
-            (6, 'Richard', 'Smith', 4, 5),
-            (7, 'Donna', 'Smith', 4, 5),
-            (8, 'Josh', 'Harrelson', null, null),
-            (9, 'Anna', 'Harrelson', 7, 8);
     EOS;
 
     try {
